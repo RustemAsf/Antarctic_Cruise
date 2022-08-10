2b27980c5859db5b315d8a2d17dad883a886a31d
Rustem <arr747@mail.ru>
2022-08-10 23:53:59 +0500

    redact tred

--- source/js/main.js
+++ source/js/main.js
@@ -9,16 +9,21 @@ const inputCheckbox = bookingForm.querySelector('#checkbox');
 
 
 // Описание логики меню
-navMain.classList.remove('main-nav--nojs');
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
+if (navMain) {
+  navMain.classList.remove('main-nav--nojs');
+}
+if (navToggle) {
+  navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+}
+
 
 const onInputNameChange = () => {
   const valueNameInput = inputName.value;
